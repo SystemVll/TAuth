c95328321fd9017babef7548e39f53e54cdd47ef
Inplex-sys <69421356+SystemVll@users.noreply.github.com>
2025-05-02 01:08:51 +0200

    Refactor code structure for improved readability and maintainability

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,58 +1,57 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ["class"],
-    content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
-  theme: {
-  	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {
-  			background: '#181818',
-  			foreground: '#c2c2c2',
-  			card: {
-  				DEFAULT: '#181818',
-  				foreground: '#c2c2c2'
-  			},
-  			popover: {
-  				DEFAULT: '#181818',
-  				foreground: '#c2c2c2',
-  			},
-  			primary: {
-  				DEFAULT: '#d3f4f8',
-  				foreground: '#181818'
-  			},
-  			secondary: {
-  				DEFAULT: '#272727',
-  				foreground: '#181818'
-  			},
-  			muted: {
-  				DEFAULT: '#272727',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: '#383838',
-  				foreground: '#c2c2c2'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: '#383838',
-  			input: '#383838',
-  			ring: 'transparent',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		}
-  	}
-  },
-  plugins: [require("tailwindcss-animate")],
-}
-
+    darkMode: ['class'],
+    content: ['./index.html', './src/**/*.{ts,tsx,js,jsx}'],
+    theme: {
+        extend: {
+            borderRadius: {
+                lg: 'var(--radius)',
+                md: 'calc(var(--radius) - 2px)',
+                sm: 'calc(var(--radius) - 4px)',
+            },
+            colors: {
+                background: '#181818',
+                foreground: '#c2c2c2',
+                card: {
+                    DEFAULT: '#181818',
+                    foreground: '#c2c2c2',
+                },
+                popover: {
+                    DEFAULT: '#181818',
+                    foreground: '#c2c2c2',
+                },
+                primary: {
+                    DEFAULT: '#d3f4f8',
+                    foreground: '#181818',
+                },
+                secondary: {
+                    DEFAULT: '#272727',
+                    foreground: '#181818',
+                },
+                muted: {
+                    DEFAULT: '#272727',
+                    foreground: 'hsl(var(--muted-foreground))',
+                },
+                accent: {
+                    DEFAULT: '#383838',
+                    foreground: '#c2c2c2',
+                },
+                destructive: {
+                    DEFAULT: 'hsl(var(--destructive))',
+                    foreground: 'hsl(var(--destructive-foreground))',
+                },
+                border: '#383838',
+                input: '#383838',
+                ring: 'transparent',
+                chart: {
+                    1: 'hsl(var(--chart-1))',
+                    2: 'hsl(var(--chart-2))',
+                    3: 'hsl(var(--chart-3))',
+                    4: 'hsl(var(--chart-4))',
+                    5: 'hsl(var(--chart-5))',
+                },
+            },
+        },
+    },
+    plugins: [require('tailwindcss-animate')],
+};

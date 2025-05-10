864a6409a940c46b5aca355b4d046b5c41496b7c
Inplex-sys <69421356+SystemVll@users.noreply.github.com>
2025-05-10 16:38:47 +0200

    feat: update window dimensions and enhance activity tracking in VaultContext

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,11 @@ import '@Assets/App.css';
 
 import ReactDOM from 'react-dom/client';
 
+import { VaultProvider } from './context/VaultContext';
 import Router from './Router';
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-	<Router />
+	<VaultProvider>
+		<Router />
+	</VaultProvider>
 );
